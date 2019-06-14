450fae67c4fba5bd7a67c8ebe202b86fb463e5ab
AndreaLupoSperanza <and.speranza@stud.uniroma3.it>
2019-06-14 18:34:42 +0200

    Capito il meccanismo delle checkbox

--- Progetto/src/main/java/it/uniroma3/siw/demospring/controller/GalleriaController.java
+++ Progetto/src/main/java/it/uniroma3/siw/demospring/controller/GalleriaController.java
@@ -1,11 +1,16 @@
 package it.uniroma3.siw.demospring.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.uniroma3.siw.demospring.model.Foto;
 import it.uniroma3.siw.demospring.repository.FotoRepository;
@@ -21,6 +26,26 @@ public class GalleriaController {
 	public String paginaInizio(Model model) {
 		List<Foto> foto = (List<Foto>) this.fotoRepository.findAll();
 		model.addAttribute("fotoVisualizzate", foto);
+		return "index";
+	}
+	
+
+	@RequestMapping(value = "/addFotoSelezionate", method = RequestMethod.POST)
+	public String prendiTutteLeFotoSelezionate(@RequestParam(value = "listaFoto") String[]  idFotoLista, 
+			Model model) {
+		List<Foto> fotoLista = new ArrayList<Foto>();
+		System.out.println("lunghezza idFotoLista: "+idFotoLista.length);
+		try {
+			for(String idFotoSingola : idFotoLista) {
+				Long id = Long.parseLong(idFotoSingola.trim());
+				fotoLista.add(this.fotoRepository.findById(id).get());
+				System.out.println("id Foto sel: "+idFotoSingola);
+			}
+		}catch (NullPointerException e) {
+			System.out.println("Foto richieste VUOTE");
+		}
+
+		model.addAttribute("fotoVisualizzate", fotoLista);
 		return "index";
 	}
 	
