78e8125e6e2b5b6c60ada6df1fe79446d60a9d9a
AndreaLupoSperanza <and.speranza@stud.uniroma3.it>
2019-06-14 15:26:44 +0200

    Modifiche

--- galleria/src/main/java/it/uniroma3/siw/controller/GalleriaController.java
+++ galleria/src/main/java/it/uniroma3/siw/controller/GalleriaController.java
@@ -1,5 +1,16 @@
 package it.uniroma3.siw.controller;
 
-public class GalleriaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class GalleriaController {
+	
+	@RequestMapping("/index")
+	public String paginaInizio(Model model) {
+		model.addAttribute("fotoVisualizzate", null);
+		return "index";
+	}
+	
 }
