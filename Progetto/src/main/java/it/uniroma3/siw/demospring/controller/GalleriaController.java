d6573b62195404f782fb69f5eea4c0bef35bb2e8
AndreaLupoSperanza <and.speranza@stud.uniroma3.it>
2019-06-14 16:19:09 +0200

    Daje

--- Progetto/src/main/java/it/uniroma3/siw/demospring/controller/GalleriaController.java
+++ Progetto/src/main/java/it/uniroma3/siw/demospring/controller/GalleriaController.java
@@ -1,18 +1,26 @@
 package it.uniroma3.siw.demospring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.uniroma3.siw.demospring.model.Foto;
+import it.uniroma3.siw.demospring.repository.FotoRepository;
 
 
 @Controller
 public class GalleriaController {
 	
-	@RequestMapping("/in")
+	 @Autowired
+	 private FotoRepository fotoRepository;
+	
+	@RequestMapping("/index")
 	public String paginaInizio(Model model) {
-		model.addAttribute("fotoVisualizzate", new Foto());
+		List<Foto> foto = (List<Foto>) this.fotoRepository.findAll();
+		model.addAttribute("fotoVisualizzate", foto);
 		return "index";
 	}
 	
