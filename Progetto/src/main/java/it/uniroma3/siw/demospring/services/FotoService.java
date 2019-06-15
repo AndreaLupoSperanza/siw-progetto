5850f77661ca0cb4bed3b715d582f10566416c75
enea <enea.cekorja@gmail.com>
2019-06-15 13:10:53 +0200

    Created controller: - list all foto - list all foto by given id

--- Progetto/src/main/java/it/uniroma3/siw/demospring/services/FotoService.java
+++ Progetto/src/main/java/it/uniroma3/siw/demospring/services/FotoService.java
@@ -1,5 +1,40 @@
 package it.uniroma3.siw.demospring.services;
 
+import it.uniroma3.siw.demospring.model.Foto;
+import it.uniroma3.siw.demospring.repository.FotoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class FotoService {
 
+    private final FotoRepository fotoRepository;
+
+    public FotoService(FotoRepository fotoRepository) {
+        this.fotoRepository = fotoRepository;
+    }
+
+    public List<Foto> findAllFoto() {
+
+        List<Foto> returnAllFoto = new ArrayList<>();
+
+        Iterable<Foto> allFoto = fotoRepository.findAll();
+
+        allFoto.forEach(returnAllFoto::add);
+
+        return returnAllFoto;
+    }
+
+    public List<Foto> findAllFoto(List<Long> fotoIds) {
+        List<Foto> returnAllFotoSelezionate = new ArrayList<>();
+
+        for (Long id : fotoIds) {
+            Foto foto = fotoRepository.findById(id).get();
+            returnAllFotoSelezionate.add(foto);
+        }
+
+        return returnAllFotoSelezionate;
+    }
 }
